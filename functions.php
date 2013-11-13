8a5ca01345a8f9c904477b1f700b98a6921552a7
Florencia Rosenfeld <hola@floristeady.com>
2013-11-12 23:14:53 -0600

    New home code and custom fields

--- functions.php
+++ functions.php
@@ -515,6 +515,99 @@ function custom_table() {
 
 add_action('admin_head', 'custom_table');
 
+
+
+/**
+********************* CUSTOM FIELDS IN PAGE FOR FRONT PAGE*****************
+*/
+function add_custom_meta_box_frontpage() {
+    add_meta_box(
+		'custom_meta_box_frontpage', // $id
+		'Publicar en Página de Inicio', // $title
+		'show_custom_meta_box_frontpage', // $callback
+		'page', // $page
+		'side', // $context
+		'default'); // $priority
+}
+
+add_action('add_meta_boxes', 'add_custom_meta_box_frontpage');
+add_action('save_post', 'save_custom_meta_frontpage');  
+
+// Field Array
+$prefix_two = 'custom_';
+$custom_meta_fields_two = array(
+	array(
+		'label'=> 'Habilitar esta opción para mostrar en página de inicio:',
+		'desc'	=> 'Contenido Destacado',
+		'id'	=> $prefix_two.'featured',
+		'type'	=> 'checkbox'
+	)
+);
+
+// The Callback
+function show_custom_meta_box_frontpage() {
+	global $custom_meta_fields_two, $post;
+
+	// Use nonce for verification
+	echo '<input type="hidden" name="custom_meta_box_nonce" value="'.wp_create_nonce(basename(__FILE__)).'" />';
+		// Begin the field table and loop
+		echo '<div class="form-table">';
+		foreach ($custom_meta_fields_two as $field) {
+			// get value of this field if it exists for this post
+			$meta = get_post_meta($post->ID, $field['id'], true);
+			// begin a table row with
+			echo '<div class="inside" style="margin-top:10px;">
+          <span id="timestamp"><label style="font-size:13px;" for="'.$field['id'].'">'.$field['label'].'</label><br/><br/>';
+					switch($field['type']) {
+										
+					// checkbox  
+					case 'checkbox': 
+
+					    echo '<input type="checkbox" name="'.$field['id'].'" id="'.$field['id'].'" ', $meta? ' checked="checked" ' : 'no','/> 
+					        <label for="'.$field['id'].'">'.$field['desc'].'</label><br/><br/>';  
+					break; 
+					
+					// text
+					case 'text':
+						echo '<input type="text" name="'.$field['id'].'" id="'.$field['id'].'" value="'.$meta.'" size="30" />
+							<br /><span class="description">'.$field['desc'].'</span>';
+					break;	
+					
+					} //end switch
+			echo '</span></div>';
+		} // end foreach
+		echo '</div>'; // end table	
+}
+
+
+// Save the Data
+function save_custom_meta_frontpage ($post_id) {
+    global $custom_meta_fields_two, $post_id, $post;
+	// verify nonce
+	if (!isset($_POST['custom_meta_box_nonce']) || !wp_verify_nonce($_POST['custom_meta_box_nonce'], basename(__FILE__)))
+		return $post_id;
+	// check autosave
+	if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)
+		return $post_id;
+	// check permissions
+	if ('page' == $_POST['post_type']) {
+		if (!current_user_can('edit_page', $post_id)){
+			return $post_id;}
+		} elseif (!current_user_can('edit_post', $post_id)) {
+			return $post_id;
+	}
+	// loop through fields and save the data
+	foreach ($custom_meta_fields_two as $field) {	
+		if ( isset( $_POST[$field['id']]) ) {
+	    	update_post_meta($post->ID, $field['id'], $_POST[$field['id']]);
+	    	//echo ($_POST[$field['id']]);
+		} else {
+		    delete_post_meta($post->ID,  $field['id']);
+		}
+	} // end foreach
+}
+
+
 /**
 ********************* CUSTOM META FIELDS PAGES*****************
 */
@@ -552,12 +645,6 @@ $custom_meta_fields = array(
 		'desc'	=> 'Agregar texto columna izquierda.',
 		'id'	=> $prefix.'right',
 		'type'	=> 'textareatiny'
-	),
-	array(
-		'label'=> 'Columna Izquierda',
-		'desc'	=> 'Agregar texto columna izquierda.',
-		'id'	=> $prefix.'thum',
-		'type'	=> 'supertextareatiny'
 	)
 );
 
@@ -643,7 +730,7 @@ function show_custom_meta_box() {
 
 // Save the Data
 function save_custom_meta_one ($post_id) {
-    global $custom_meta_fields;
+    global $custom_meta_fields, $post_id;
 	// verify nonce
 	if (!isset($_POST['custom_meta_box_nonce']) || !wp_verify_nonce($_POST['custom_meta_box_nonce'], basename(__FILE__)))
 		return $post_id;
@@ -664,86 +751,4 @@ function save_custom_meta_one ($post_id) {
 		update_post_meta($post_id, $field['id'], $new);
 	} // end foreach
 }
-
-
-
-/**
-********************* CUSTOM FIELDS IN PAGE FOR FRONT PAGE*****************
-*/
-function add_custom_meta_box_frontpage() {
-    add_meta_box(
-		'custom_meta_box_frontpage', // $id
-		'Publicar en Página de Inicio', // $title
-		'show_custom_meta_box_frontpage', // $callback
-		'page', // $page
-		'side', // $context
-		'default'); // $priority
-}
-
-add_action('add_meta_boxes', 'add_custom_meta_box_frontpage');
-add_action('save_post', 'save_custom_meta');  
-
-// Field Array
-$prefix = 'custom_';
-$custom_meta_fields_two = array(
-	array(
-		'label'=> 'Habilitar esta opción para mostrar en página de inicio:',
-		'desc'	=> 'Contenido Destacado',
-		'id'	=> $prefix.'featured_page',
-		'type'	=> 'checkbox'
-	)
-);
-
-// The Callback
-function show_custom_meta_box_frontpage() {
-	global $custom_meta_fields_two, $post;
-	// Use nonce for verification
-	echo '<input type="hidden" name="custom_meta_box_nonce" value="'.wp_create_nonce(basename(__FILE__)).'" />';
-		// Begin the field table and loop
-		echo '<div class="form-table">';
-		foreach ($custom_meta_fields_two as $field) {
-			// get value of this field if it exists for this post
-			$meta = get_post_meta($post->ID, $field['id'], true);
-			// begin a table row with
-			echo '<div class="inside" style="margin-top:10px;">
-          <span id="timestamp"><label style="font-size:13px;" for="'.$field['id'].'">'.$field['label'].'</label><br/><br/>';
-					switch($field['type']) {
-										
-					// checkbox  
-					case 'checkbox':  
-					    echo '<input type="checkbox" name="'.$field['id'].'" id="'.$field['id'].'" ',$meta ? ' checked="checked"' : '','/> 
-					        <label for="'.$field['id'].'">'.$field['desc'].'</label><br/><br/>';  
-					break; 
-					
-					} //end switch
-			echo '</span></div>';
-		} // end foreach
-		echo '</div>'; // end table	
-}
-
-
-// Save the Data
-function save_custom_meta($post_id) {
-    global $custom_meta_fields_two;
-	// verify nonce
-	if (!isset($_POST['custom_meta_box_nonce']) || !wp_verify_nonce($_POST['custom_meta_box_nonce'], basename(__FILE__)))
-		return $post_id;
-	// check autosave
-	if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)
-		return $post_id;
-	// check permissions
-	if ('post' == $_POST['post_type']) {
-		if (!current_user_can('edit_page', $post_id)){
-			return $post_id;}
-		} elseif (!current_user_can('edit_post', $post_id)) {
-			return $post_id;
-	}
-	// loop through fields and save the data
-	foreach ($custom_meta_fields_two as $field) {
-		$old = get_post_meta($post_id, $field['id'], true);
-		$new = $_POST[$field['id']];
-		update_post_meta($post_id, $field['id'], $new);
-	} // end foreach
-}
-
 ?>
--- home.php
+++ home.php
@@ -11,29 +11,173 @@ get_header(); ?>
 
 <div id="content">
 
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<h1 class="entry-title"><?php the_title(); ?></h1>
-					<div class="entry-meta">
-						<?php geobiota_posted_on(); ?>
-					</div><!-- .entry-meta -->
-					<div class="entry-content">
-						<?php the_content(); ?>
-						<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'geobiota' ), 'after' => '' ) ); ?>
-					</div><!-- .entry-content -->
-<?php if ( get_the_author_meta( 'description' ) ) : // If a user has filled out their description, show a bio on their entries  ?>
+<article style="float:left;">
 
-<?php endif; ?>
-   				   <?php comments_template( '', true ); ?>
- 					
-					<footer class="entry-utility">
-						<?php geobiota_posted_in(); ?>
-					</footer><!-- .entry-utility -->
-					   <?php edit_post_link( __( 'Edit', 'geobiota' ), '<span class="edit-link">', '</span>' ); ?>
-				</article><!-- #post-## -->
+<?php global $geobiota_options;
+						$geobiota_settings = get_option( 'geobiota_options', $geobiota_options ); ?>
+<?php if( $geobiota_settings['custom_logo'] ) : ?>
+						<h1><a href="<?php echo bloginfo('url'); ?>" class="logo"><img src="<?php echo $geobiota_settings['custom_logo']; ?>" alt="<?php bloginfo('name'); ?>" /> </a></h1>
+						<?php endif; ?>
+</article>
 
+
+<!---loop1-->
+	<?php $args = array(
+			'post_type'	=> 'page',
+			'post_status' => 'publish',
+			'posts_per_page' => 2,
+			'orderby' => 'menu_order',
+			'order' => 'ASC',
+			'offset' => 0,
+			'meta_query' => array(
+				array( 'key' => 'custom_featured', 'value' => 'on')
+			) );
+		$page_featured = new WP_Query( $args );
+	?>
+	
+	<?php $i = 1 ; ?>	
+	<?php if ( $page_featured->have_posts() ) while ( $page_featured->have_posts() ) : $page_featured->the_post(); ?>
+	
+	<?php if ($i == 1): ?>
+	<article id="article-1" class="post-1 post-<?php the_ID(); ?> two_thirds">
+		<p>page1</p>
+		<div class="page-featured-content">
+			<?php 		
+			 $content = $post->post_excerpt;
+			 $content = strip_tags($content);
+			 $content = substr($content, 0, 120);
+			 echo '<h2>' . $content . '<h2>'; 
+			?>
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+		</div><!-- .entry-content -->
+
+		 <span class="img"><?php echo get_the_post_thumbnail( $post->ID, array(370,364), array('class' => 'product-image')); ?></span>
+		 
+	</article><!-- #post-## -->
+	<?php elseif ($i == 2) :?>
+	
+	<article id="article-2" class="post-2 post-<?php the_ID(); ?> one_third">
+	   <p>foto page2</p>
+		<span class="img"><?php echo get_the_post_thumbnail( $post->ID, array(370,483), array('class' => 'product-image')); ?></span>
+	</article><!-- #post-## -->
+	
+	<?php endif; ?>
+	
+<?php  $i++; endwhile; // end of the loop. ?>
+
+<!---loop2-->
+
+<?php $args = array(
+			'post_type'	=> 'page',
+			'post_status' => 'publish',
+			'posts_per_page' => 2,
+			'orderby' => 'menu_order',
+			'order' => 'ASC',
+			'offset' => 1,
+			'meta_query' => array(
+				array( 'key' => 'custom_featured', 'value' => 'on')
+			) );
+		$page_featured = new WP_Query( $args );
+	?>
+	
+	<?php $i = 1 ; ?>	
+	<?php if ( $page_featured->have_posts() ) while ( $page_featured->have_posts() ) : $page_featured->the_post(); ?>
+	
+	<?php if ($i == 1): ?>
+	<article id="article-3" class="post-2 post-<?php the_ID(); ?> one_third" >
+		<p>texto page2</p>
+		<div class="page-featured-content">
+			<?php 		
+			 $content = $post->post_excerpt;
+			 $content = strip_tags($content);
+			 $content = substr($content, 0, 120);
+			 echo '<h2>' . $content . '<h2>'; 
+			?>
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+		</div><!-- .entry-content -->
+		 
+	</article><!-- #post-## -->
+	<?php elseif ($i == 2) :?>
+	
+	<article id="article-4" class="post-3 post-<?php the_ID(); ?> one_third">
+		<p>foto page3</p>
+		<span class="img"><?php echo get_the_post_thumbnail( $post->ID, array(370,364), array('class' => 'product-image')); ?></span>
+	</article><!-- #post-## -->
+	
+	<?php endif; ?>
+	
+<?php $i++; endwhile; // end of the loop. ?>
+
+<!---loop3-->
+
+<?php $args = array(
+			'post_type'	=> 'page',
+			'post_status' => 'publish',
+			'posts_per_page' => 1,
+			'orderby' => 'menu_order',
+			'order' => 'ASC',
+			'offset' => 2,
+			'meta_query' => array(
+				array( 'key' => 'custom_featured', 'value' => 'on')
+			) );
+		$page_featured = new WP_Query( $args );
+	?>
+		
+	<?php if ( $page_featured->have_posts() ) while ( $page_featured->have_posts() ) : $page_featured->the_post(); ?>
+	
+	<article id="article-5" class="post-3 post-<?php the_ID(); ?> one_third">
+		<p>texto page3</p>
+		<div class="page-featured-content">
+			<?php 		
+			 $content = $post->post_excerpt;
+			 $content = strip_tags($content);
+			 $content = substr($content, 0, 120);
+			 echo '<h2>' . $content . '<h2>'; 
+			?>
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+		</div><!-- .entry-content -->
+		 
+	</article><!-- #post-## -->
+	
 <?php endwhile; // end of the loop. ?>
+
+<!---loop4-->
+	<?php $args = array(
+			'post_type'	=> 'page',
+			'post_status' => 'publish',
+			'posts_per_page' => 1,
+			'orderby' => 'menu_order',
+			'order' => 'ASC',
+			'offset' => 3,
+			'meta_query' => array(
+				array( 'key' => 'custom_featured', 'value' => 'on')
+			) );
+		$page_featured = new WP_Query( $args );
+	?>
+	
+	<?php if ( $page_featured->have_posts() ) while ( $page_featured->have_posts() ) : $page_featured->the_post(); ?>
+	
+	<article id="article-6" class="post-4 post-<?php the_ID(); ?>">
+	
+		<p>page4</p>	
+		 <span class="img"><?php echo get_the_post_thumbnail( $post->ID, array(370,364), array('class' => 'product-image')); ?></span>
+		
+		<div class="page-featured-content">
+			<?php 		
+			 $content = $post->post_excerpt;
+			 $content = strip_tags($content);
+			 $content = substr($content, 0, 120);
+			 echo '<h2>' . $content . '<h2>'; 
+			?>
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+		</div><!-- .entry-content -->
+	 
+	</article><!-- #post-## -->
+	
+<?php endwhile; // end of the loop. ?>
+
+
 
 </div><!--#content-->
 
